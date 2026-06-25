4bce796f421594bbff4021cabd0c80352669cbc0
lu23222 <luciana.maresca6@gmail.com>
2026-06-25 18:17:30 +0200

    modifica interfaccia codice sconto dao

--- src/main/java/dao/CodiceScontoDao.java
+++ src/main/java/dao/CodiceScontoDao.java
@@ -15,5 +15,7 @@ public interface CodiceScontoDao {
 	public CodiceScontoBean doRetrieveByKey(String code_id) throws SQLException;
 	
 	public ArrayList<CodiceScontoBean> doRetrieveAll() throws SQLException;
+
+	public boolean doUpdateStato(String code_id, boolean stato) throws SQLException;
 	
 }
