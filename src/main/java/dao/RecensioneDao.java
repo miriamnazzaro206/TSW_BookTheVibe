f4eaf2d68ad7f7f90af8a946d4d9e67ff26b4d70
lu23222 <luciana.maresca6@gmail.com>
2026-06-25 18:32:54 +0200

    modifica dao

--- src/main/java/dao/RecensioneDao.java
+++ src/main/java/dao/RecensioneDao.java
@@ -10,5 +10,7 @@ public interface RecensioneDao {
 	public void doSave(RecensioneBean recensione) throws SQLException;
 
 	public  ArrayList <RecensioneBean> doRetrieveByKey(int idAttivita) throws SQLException;
+	
+	public RecensioneBean doRetrieveByAttivitaAndUtente(int idAttivita, int idUtente) throws SQLException;
 
 }
