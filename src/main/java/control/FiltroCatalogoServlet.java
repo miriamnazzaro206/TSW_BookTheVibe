66df08adefa31853039617e3cfebb1592fe4ed07
miriamnazzaro206 <miriamnazzaro@gmail.com>
2026-07-02 13:21:20 +0200

    filtrocatalogoservlet modificato

--- src/main/java/control/FiltroCatalogoServlet.java
+++ src/main/java/control/FiltroCatalogoServlet.java
@@ -14,6 +14,7 @@ import model.AttivitaBean;
 
 @WebServlet("/common/catalogo/filtra")
 public class FiltroCatalogoServlet extends BaseServlet {
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
